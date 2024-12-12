437aea2857ba511fa5c0ea5e62aed83e2679df90
Woncest <107243849+Woncest@users.noreply.github.com>
2024-12-12 13:32:39 +0100

    even better ? but running out of options
    Obviously needs to be put into a loop was just quicker for testing if it even works

--- OwnTakeAtWFC/Assets/TileGridGenerator.cs
+++ OwnTakeAtWFC/Assets/TileGridGenerator.cs
@@ -207,14 +207,25 @@ public class TileGridGenerator : MonoBehaviour
                 List<GameObject> tilesRight = selectedTilePrefab.GetComponent<Tile>().allowedAbove;
                 bool hasStreetStraight = tilesRight.Any(tile => tile.name == "Street_Straight");
 
-                if (hasStreetStraight)
+                if (hasStreetStraight && selectedTilePrefab.gameObject.name != "Street_Empty")
                 {
                     
                     UnityEngine.Debug.Log("Street_Straight found in tilesRight at x:" + x + " y:" + y + " IsAheadClear was: " + IsAheadClear(x,y,3));
                     // Perform desired action
-                    if(!IsAheadClear(x,y,3)){
+                    if(!IsAheadClear(x,y,3) && selectedTilePrefab.gameObject.name != "Street_Empty"){
                         UnityEngine.Debug.Log("Reroll");
+                        cellGrid[x,y].possibleTiles.Remove(selectedTilePrefab);
                         selectedTilePrefab = cellGrid[x, y].possibleTiles[Random.Range(0, cellGrid[x, y].possibleTiles.Count)];
+                            if(!IsAheadClear(x,y,3) && selectedTilePrefab.gameObject.name != "Street_Empty"){
+                                UnityEngine.Debug.Log("Reroll");
+                                cellGrid[x,y].possibleTiles.Remove(selectedTilePrefab);
+                                selectedTilePrefab = cellGrid[x, y].possibleTiles[Random.Range(0, cellGrid[x, y].possibleTiles.Count)];
+                                if(!IsAheadClear(x,y,3) && selectedTilePrefab.gameObject.name != "Street_Empty"){
+                                    UnityEngine.Debug.Log("Reroll");
+                                    cellGrid[x,y].possibleTiles.Remove(selectedTilePrefab);
+                                    selectedTilePrefab = cellGrid[x, y].possibleTiles[Random.Range(0, cellGrid[x, y].possibleTiles.Count)];
+                                }
+                        }
                     }
                 }
 
