46787beea7c2777ebf9008ba49e5ace820878d19
Woncest <107243849+Woncest@users.noreply.github.com>
2025-01-12 21:31:03 +0100

    this works , not really grid but can be made into a gird

---
+++ OwnTakeAtWFC/Assets/BlockGenerator.cs
@@ -0,0 +1,122 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockGenerator : MonoBehaviour
+{
+    public int defaultWidth = 2; // Default block width
+    public int blockHeight = 2; // Default block height
+
+    private HashSet<Vector3> nodes = new HashSet<Vector3>();
+    private HashSet<(Vector3, Vector3)> edges = new HashSet<(Vector3, Vector3)>();
+
+    void Start()
+    {
+        GenerateFilledGrid(25, 25);
+        OutputEdgeDimensions();
+    }
+
+    void GenerateFilledGrid(int minWidth, int minHeight)
+    {
+        int gridWidth = Mathf.CeilToInt((float)minWidth / defaultWidth) * defaultWidth;
+        int gridHeight = Mathf.CeilToInt((float)minHeight / blockHeight) * blockHeight;
+
+        for (int y = 0; y < gridHeight; y += blockHeight)
+        {
+            for (int x = 0; x < gridWidth; x += defaultWidth)
+            {
+                GenerateBlock(new Vector3(x, y, 0), defaultWidth, blockHeight);
+            }
+        }
+    }
+
+    void GenerateBlock(Vector3 position, int width, int height)
+    {
+        // Generate corner nodes
+        AddNode(position + new Vector3(0, height, 0)); // Top-left
+        AddNode(position + new Vector3(width, height, 0)); // Top-right
+        AddNode(position); // Bottom-left
+        AddNode(position + new Vector3(width, 0, 0)); // Bottom-right
+
+        // Generate side nodes (every 1 unit along the edges)
+        for (int x = 1; x < width; x++)
+        {
+            AddNode(position + new Vector3(x, 0, 0)); // Bottom edge
+            AddNode(position + new Vector3(x, height, 0)); // Top edge
+        }
+
+        for (int y = 1; y < height; y++)
+        {
+            AddNode(position + new Vector3(0, y, 0)); // Left edge
+            AddNode(position + new Vector3(width, y, 0)); // Right edge
+        }
+
+        // Connect edges only between directly adjacent nodes
+        CreateEdges(position, width, height);
+    }
+
+    void AddNode(Vector3 node)
+    {
+        nodes.Add(node);
+    }
+
+    void CreateEdges(Vector3 position, int width, int height)
+    {
+        // Bottom and top edges
+        for (int x = 0; x < width; x++)
+        {
+            ConnectNodes(position + new Vector3(x, 0, 0), position + new Vector3(x + 1, 0, 0)); // Bottom
+            ConnectNodes(position + new Vector3(x, height, 0), position + new Vector3(x + 1, height, 0)); // Top
+        }
+
+        // Left and right edges
+        for (int y = 0; y < height; y++)
+        {
+            ConnectNodes(position + new Vector3(0, y, 0), position + new Vector3(0, y + 1, 0)); // Left
+            ConnectNodes(position + new Vector3(width, y, 0), position + new Vector3(width, y + 1, 0)); // Right
+        }
+    }
+
+    void ConnectNodes(Vector3 start, Vector3 end)
+    {
+        if (nodes.Contains(start) && nodes.Contains(end))
+        {
+            edges.Add((start, end));
+        }
+    }
+
+    void OutputEdgeDimensions()
+    {
+        float totalHorizontal = 0;
+        float totalVertical = 0;
+
+        foreach (var edge in edges)
+        {
+            if (Mathf.Abs(edge.Item1.y - edge.Item2.y) < 0.01f) // Horizontal edge
+            {
+                totalHorizontal += Vector3.Distance(edge.Item1, edge.Item2);
+            }
+            else if (Mathf.Abs(edge.Item1.x - edge.Item2.x) < 0.01f) // Vertical edge
+            {
+                totalVertical += Vector3.Distance(edge.Item1, edge.Item2);
+            }
+        }
+
+        Debug.Log($"Total Horizontal Length (one side): {totalHorizontal}");
+        Debug.Log($"Total Vertical Length (one side): {totalVertical}");
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        foreach (var node in nodes)
+        {
+            Gizmos.DrawSphere(node, 0.1f); // Draw nodes as small spheres
+        }
+
+        Gizmos.color = Color.blue;
+        foreach (var edge in edges)
+        {
+            Gizmos.DrawLine(edge.Item1, edge.Item2); // Draw edges as lines
+        }
+    }
+}
